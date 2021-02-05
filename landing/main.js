0e933889ea14b8d49adb594b1283afcce01e41d1
brymo <bryanmoh@yahoo.com.au>
2021-02-05 12:09:08 +1100

    forgot to add

---
+++ landing/main.js
@@ -0,0 +1,5 @@
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
